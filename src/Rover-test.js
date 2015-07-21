1a85d828f002c8bd0d24981d3fa6b11b74c598f9
Nick Hayes <nhayes559@gmail.com>
2015-07-21 14:07:38 +0100

    I've added a really simple jasmine test (rover-test) and a rover object. The test just checks if the rover is true, and rover itself just exports true. Should mention that I set up my karma config with browserify and jasmine. My process is to just CTRL+R in Intellij to rerun after I've updated a test.

--- src/Rover-test.js
+++ src/Rover-test.js
@@ -1,3 +1,10 @@
 /**
  * Created by nick on 21/07/2015.
  */
+var Rover = require('./Rover');
+
+describe("a test", function() {
+    it("should work", function () {
+        expect(Rover).toBe(true);
+    });
+});
--- src/Rover.js
+++ src/Rover.js
@@ -1,3 +1,5 @@
 /**
  * Created by nick on 21/07/2015.
  */
+var Rover = true;
+module.exports = Rover;
