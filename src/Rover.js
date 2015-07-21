d62b1fb033e8fd65744b52f42015b117898d21c8
Nick Hayes <nhayes559@gmail.com>
2015-07-21 14:22:52 +0100

    Started flushing out the rover object, with move and setup commands. Initially did a switch on direction, but I'm going to move this into if branches because of the complexity of R and L commands on direction. Almost done.

--- src/Rover.js
+++ src/Rover.js
@@ -2,6 +2,25 @@
  * Created by nick on 21/07/2015.
  */
 
-
-var Rover = {};
+var Rover = {
+    'position' : [0,0,'N'],
+    'setup' : function(config){
+        this.position = config;
+    },
+    'move' : function(moveString){
+        for ( var i = 0; i < moveString.length; i++ ) {
+            var command = moveString.charAt(i);
+            var xPos = this.position[0];
+            var yPos = this.position[1];
+            var direction = this.position[2];
+            switch(command){
+                case 'F' : this.position = [xPos, yPos + 1, direction]; break;
+                case 'R' : this.position = [xPos + 1, yPos, 'E']; break;
+                case 'B' : this.position = [xPos + 1, yPos - 1, 'S']; break;
+                case 'L' : this.position = [xPos - 1, yPos, 'W']; break;
+            }
+        }
+        return this.position;
+    }
+};
 module.exports = Rover;
